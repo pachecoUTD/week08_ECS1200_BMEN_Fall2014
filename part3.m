82b2040d5778e959ec3e55827db021b037cd304a
Joe Pacheco <joe.pacheco@utdallas.edu>
2014-10-08 22:56:37 -0500

    Initial commit

---
+++ part3.m
@@ -0,0 +1,13 @@
+% Write code that turns on motor E, waits two seconds, turns on motor A 
+% for 2 seconds while the motor E continues to run, changes the direction 
+% of motor A for 2 more seconds, stops motor A, then reverses motor A for 
+% 1 second, then stops motors A and E.  
+
+% Write your code below here:
+
+
+
+
+
+
+
---
+++ test_motors.m
@@ -0,0 +1,58 @@
+% Before running this code, connect to the Arduino using the command
+% a = arduino('COM3');
+% Note: your COM port number may be different
+
+%% Initialize motor5
+initialize_motor5
+
+% Note: Motors #1 - #4 are initialized when you connect to the arduino
+% using the a = arduino('COM3') command. Because motor #5 is controlled by
+% an external IC (i.e., not on the motor shield), it needs to be
+% initialized separately.
+
+
+%% Motor test code
+
+% Length of time to pause in between motor commands. This effects how long
+% the motor actually runs. See the code below for more details.
+
+motorID = 5;
+
+% This command sets the speed of motor 'motorID'. The motorSpeed variable
+% should be an integer between 0 and 255
+motorSpeed = 255;
+motor5 = motorController(a, motor5, motorID, 'speed', motorSpeed);
+
+% This command runs motor 'motorID' forward.
+motor5 = motorController(a, motor5, motorID, 'forward');
+
+% After starting the motor, we pause for the length of time we want the
+% motor to run
+pause(2);
+
+% After the pause, we issue a 'release' command to stop the motor
+motor5 = motorController(a, motor5, motorID, 'release');
+
+% We now pause again to give some time between moving foward and
+% backward
+pause(1);
+
+% We now tell the motor to move backward
+motor5 = motorController(a, motor5, motorID, 'backward');
+
+% Again we pause for the length of time we want the motor to run
+pause(5);
+
+% After running the motor backward, we now stop it
+motor5 = motorController(a, motor5, motorID, 'release');
+
+
+
+
+
+
+
+
+
+
+
